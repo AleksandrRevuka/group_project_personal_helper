b1cf176bd830c0b1a6f881b15c8a2e49f06659ce
RomaLoma91 <lomachinskiy.r@gmail.com>
2023-06-05 22:23:15 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='group_project_personal_helper',
+    version='0.1',
+    data_files=[('', ['README.md'])],
+    description='the bot collects information entered by clients and stores it in the directory, it can also display it on the screen if necessary',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+      ],
+    authors = ['Oleksandr Revuka', 'Oleksandr Shevchenko' , 'Evgen Kulik', 'Roman Lomachinskiy', 'Oleksii Chaika', 'Artem Ivanina'],
+    license='MIT',
+    python_requires='>=3.11',
+    entry_points={'console_scripts': ['pbot = personal_helper.run_bot:start']}    
+)
